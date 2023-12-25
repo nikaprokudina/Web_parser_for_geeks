2ad1fd2d254de8f0a2ef2e1a9c2d926b91a5a142
Fenderov Andrey <dreosashes@gmail.com>
2023-12-25 07:58:26 +0300

    update load script

--- api/books_mongo_load.py
+++ api/books_mongo_load.py
@@ -1,9 +1,11 @@
 import pymongo
 import csv
+import sys
 from os import environ as env
 from dotenv import load_dotenv
 
 load_dotenv()
+csv.field_size_limit(sys.maxsize)
 mongo_client = pymongo.MongoClient(env['CONNECTION_STRING'])
 db = mongo_client['GeekParser']
 books = db['books']
@@ -11,6 +13,10 @@ db['books'].drop()
 data = open('books_info.csv', 'r')
 reader = csv.DictReader(data)
 for row in reader:
+    try:
+        row['rating'] = float(row['rating'])
+    except Exception as ex:
+        row['rating'] = 0
     row['genres'] = row['genres'].split(sep=', ')
     books.insert_one(row)
 books.create_index({ 'title': 'text', 'author': 'text', 'comments': 'text' })
